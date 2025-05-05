81b8e4e8467f89cb6b8e6d3d4d421b0ecb6ddae3
mayis <mayisqarayev75@gmail,com>
2025-05-05 04:00:14 +0400

    User Controller

--- src/main/java/com/foody/foody_project/controller/UserController.java
+++ src/main/java/com/foody/foody_project/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.foody.foody_project.controller;
 
+import com.foody.foody_project.dto.request.UpdateUserPhoneRequestDto;
+import com.foody.foody_project.dto.request.UpdateUserRequestDto;
+import com.foody.foody_project.dto.response.UserResponseDto;
 import com.foody.foody_project.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -12,5 +14,20 @@ public class UserController {
 
     public UserController(UserService service) {
         this.service = service;
+    }
+
+    @PutMapping("/update/profile")
+    public void updateProfile(@RequestBody UpdateUserRequestDto requestDto) {
+        service.updateProfile(requestDto);
+    }
+
+    @PatchMapping("/update/phone")
+    public void updateProfilePhone(@RequestBody UpdateUserPhoneRequestDto requestDto) {
+        service.updateProfilePhone(requestDto);
+    }
+
+    @GetMapping("/profile/{id}")
+    public UserResponseDto getProfile(@PathVariable String id) {
+        return service.getProfile(id);
     }
 }
